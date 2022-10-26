d92c921bae0eb0984785bf2234ccedc4101ebe61
Lukman Nakib <48018765+nkb-bd@users.noreply.github.com>
2022-10-26 16:51:44 +0600

    Update landing layout Editor Css

--- resources/assets/admin/components/settings/LandingPage/_Browser.vue
+++ resources/assets/admin/components/settings/LandingPage/_Browser.vue
@@ -20,67 +20,88 @@
             </div>
             <div style="text-align: center; margin-top: 20px;">
                 <el-switch
-                    v-model="device_type"
-                    active-value="desktop"
-                    inactive-value="mobile"
-                    active-text="Desktop"
-                    inactive-text="Mobile">
+                        v-model="device_type"
+                        active-value="desktop"
+                        inactive-value="mobile"
+                        active-text="Desktop"
+                        inactive-text="Mobile">
                 </el-switch>
             </div>
         </div>
     </div>
 </template>
 <script type="text/babel">
-export default {
-    name: 'FFBrowserFrame',
-    data() {
-        return {
-            device_type: 'desktop',
-            loading_iframe: true,
-            frame: null
-        }
-    },
-    watch: {
-        settings: {
-            handler(settings) {
-                this.generateCss(settings);
-            },
-            deep: true
-        }
-    },
-    props: ['settings', 'preview_url'],
-    methods: {
-        initIframe() {
-            const that = this;
-            this.iframe = jQuery('<iframe/>', {
-                id: 'fcc_design_preview',
-                src: this.preview_url,
-                style: 'display:none;width:100%;height:600px',
-                load: function () {
-                    const frame = jQuery(this);
-                    frame.show();
-                  //  that.generateCss(that.design_settings);
-                    that.loading_iframe = false;
-                }
-            });
-
-            jQuery('#fcc_iframe_holder').html(this.iframe);
-        },
-        generateCss(settings) {
-            let css = '';
-            let prefix = '.ff_landing_wrapper';
-            css += `.ff_landing_page_body { background-color: ${settings.custom_color} !important; }`;
-            console.log(css);
-            this.pushCSS(css);
-        },
-        pushCSS(css) {
-            if (this.iframe) {
-                this.iframe.contents().find('head').find('#ff_landing_css').html(css);
+    export default {
+        name: 'FFBrowserFrame',
+        data() {
+            return {
+                device_type: 'desktop',
+                loading_iframe: true,
+                frame: null
             }
+        },
+        watch: {
+            settings: {
+                handler(settings) {
+                    this.generateCss(settings);
+                },
+                deep: true
+            }
+        },
+        props: ['settings', 'preview_url'],
+        methods: {
+            initIframe() {
+                const that = this;
+                this.iframe = jQuery('<iframe/>', {
+                    id: 'fcc_design_preview',
+                    src: this.preview_url,
+                    style: 'display:none;width:100%;height:600px',
+                    load: function () {
+                        const frame = jQuery(this);
+                        frame.show();
+                        //  that.generateCss(that.design_settings);
+                        that.loading_iframe = false;
+                    }
+                });
+
+                jQuery('#fcc_iframe_holder').html(this.iframe);
+            },
+            generateCss(settings) {
+                let css = '';
+                let prefix = '.ff_landing_wrapper';
+                css += `.ff_landing_page_body { background-color: ${settings.custom_color} !important; }`;
+                css += this.brightness(settings);
+                css += this.imagePositionCSS(settings);
+
+                this.pushCSS(css);
+            },
+            brightness(settings) {
+                const setValue = settings.brightness;
+                if (!setValue) {
+                    return '';
+                }
+                let css = '';
+                if (setValue > 0) {
+                    css += 'contrast(' + ((100 - setValue) / 100).toFixed(2) + ') ';
+                }
+                css += 'brightness(' + (1 + settings.brightness / 100) + ')';
+                return `.ff_conv_media_holder .fcc_block_media_attachment {filter: ${css}!important;})`;
+
+            },
+            imagePositionCSS(settings) {
+                if (settings.layout == 'media_right_full' || settings.layout == 'media_left_full') {
+                    return `.ff_conv_media_holder .fc_image_holder img {object-position: ${settings.media_x_position}% ${settings.media_y_position}%!important;}`;
+                }
+                return '';
+            },
+            pushCSS(css) {
+                if (this.iframe) {
+                    this.iframe.contents().find('head').find('#ff_landing_css').html(css);
+                }
+            }
+        },
+        mounted() {
+            this.initIframe();
         }
-    },
-    mounted() {
-        this.initIframe();
     }
-}
 </script>
